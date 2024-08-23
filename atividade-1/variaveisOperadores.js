390e1b5cacb9baae5ac8f99297fbad13a0acdb9e
CAIO VINICIUS RODRIGUES MOREIRA <caio.vmoreira@a.ucb.br>
2024-08-23 09:37:57 -0300

    atividade-1

---
+++ atividade-1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 3;
+let c;
+
+c = a + b;
+console.log(`A soma entre ${a} e ${b} é ${c}`);
+
+c = a - b;
+console.log(`A subtração entre ${a} e ${b} é ${c}`);
+
+c = a * b;
+console.log(`A multiplicação entre ${a} e ${b} é ${c}`);
+
+c = a / b;
+console.log(`A divisão entre ${a} e ${b} é ${c}`);
